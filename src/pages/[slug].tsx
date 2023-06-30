14807154021a1eb0328ef6e8284c703f6f529aa5
johannpereze <sebas.jsp@gmail.com>
2023-06-29 20:11:30 -0500

    improved the user profile styles

--- src/pages/[slug].tsx
+++ src/pages/[slug].tsx
@@ -16,13 +16,27 @@ const ProfilePage: NextPage<{ username: string }> = ({ username }) => {
         <title>{data.username}</title>
       </Head>
       <PageLayout>
-        <div>{data.username}</div>
+        <div className="relative h-36  bg-slate-600">
+          <Image
+            src={data.profileImageUrl}
+            alt={`${data.username || ""}'s profile pic`}
+            width={128}
+            height={128}
+            className="absolute bottom-0 left-0 -mb-[64px] ml-4 rounded-full border-4 border-black bg-black"
+          />
+        </div>
+        <div className="h-[64px]" />
+        <div className="p-4 text-2xl font-bold">{`@${
+          data.username || ""
+        }`}</div>
+        <div className="w-full border-b border-slate-400"></div>
       </PageLayout>
     </>
   );
 };
 
 import { createServerSideHelpers } from "@trpc/react-query/server";
+import Image from "next/image";
 import superjson from "superjson";
 import { PageLayout } from "~/components/layout";
 import { appRouter } from "~/server/api/root";
